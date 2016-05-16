923beb9e833d3fa85150f7010a09e974bc61646f
shuoyangd <shuoyangd@gmail.com>
2016-05-16 17:47:07 -0400

    add nbest list script

--- scripts/get_nbest_with_align.sh
+++ scripts/get_nbest_with_align.sh
@@ -1,5 +1,10 @@
-#
-/bin/sh
+#/bin/sh
 set -e
-/home/pkoehn/moses/scripts/generic/moses-parallel.pl -queue-parameters "-l 'arch=*64,mem_free=50G,ram_free=50G' -pe smp 12" -decoder /home/pkoehn/moses/bin/moses.2015-03-23 -config /home/pkoehn/experiment/wmt15-fr-en/evaluation/newstest2013.filtered.ini.1 -input-file /home/pkoehn/experiment/wmt15-fr-en/evaluation/newstest2013.input.tc.1 --jobs 5 -decoder-parameters "-mbr -mp -search-algorithm 1 -cube-pruning-pop-limit 5000 -s 5000 -threads 20 -max-trans-opt-per-coverage 100 -t" > tmp
+# INPUT_FILE="/home/shuoyangd/experiments/wmt16-de-en/tuning/input.split.1"
+INPUT_FILE="/home/shuoyangd/experiments/wmt16-ro-en/tuning/input.tc.1.10"
+# CONFIG_FILE="/home/shuoyangd/experiments/wmt16-de-en/tuning/moses.tuned.ini.1"
+CONFIG_FILE="/home/shuoyangd/experiments/wmt16-ro-en/tuning/moses.tuned.ini.1"
+OUTPUT_FILE=$1
+EMAIL=$2
+/home/shuoyangd/mosesstd/scripts/generic/moses-parallel.pl -queue-parameters "-l 'hostname=b*,arch=*64,mem_free=140G,ram_free=140G' -pe smp 20 -M $EMAIL" -decoder /home/shuoyangd/mosesstd/bin/moses -config $CONFIG_FILE -cache-model /mnt/data/$USER/cache -input-file $INPUT_FILE --jobs 4 -decoder-parameters "-mbr -mp -search-algorithm 1 -cube-pruning-pop-limit 5000 -s 5000 -threads 24 -max-trans-opt-per-coverage 100 -t -n-best-list $OUTPUT_FILE 200 -print-alignment-info-in-n-best" > $OUTPUT_FILE.1best
 
