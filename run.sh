6b22b93e9455b5391c97fb311006ae520cad7569
arendu <adithya.r@gmail.com>
2016-08-29 17:41:01 -0400

    old stuff

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 set -e
-python nplm.py --training-file ./corpus/correct.tok --working-dir ./tmp 
+#python nplm.py --training-file ./corpus/correct.tok --working-dir ./tmp 
+python nnjm.py --training-file ./corpus/correct.tok --souce-file  ./corpus/ --alignment--working-dir ./tmp 
