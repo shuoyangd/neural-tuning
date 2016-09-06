49cd1fba44592aa591538ef18d6378bddb63aa59
shuoyangd <shuoyangd@gmail.com>
2016-09-06 15:15:47 -0400

    fix dict index sync problem

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-#python nplm.py --training-file ./corpus/correct.tok --working-dir ./tmp 
-python nnjm.py --training-file ./corpus/correct.tok --souce-file  ./corpus/ --alignment--working-dir ./tmp 
---
+++ scripts/build_nnjm_gpu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source /home/shuoyangd/pyenv/theano/bin/activate
+cd /home/shuoyangd/neural-tuning
+# python nnjm.py --target-file /home/shuoyangd/experiments/wmt16-ro-en/training/corpus.2.en --source-file /home/shuoyangd/experiments/wmt16-ro-en/training/corpus.2.ro --alignment-file /home/shuoyangd/experiments/wmt16-ro-en/model/aligned.2.grow-diag-final-and --working-dir /home/shuoyangd/neural-tuning/ro-en.baseline --vocab-size 16000 # --max-epoch 1
+python nnjm.py --target-file /home/shuoyangd/neural-tuning/corpus/toy/toy.1best --source-file /home/shuoyangd/neural-tuning/corpus/toy/toy.src --alignment-file /home/shuoyangd/neural-tuning/corpus/toy/toy.1best.align --working-dir /home/shuoyangd/neural-tuning/ro-en.baseline --vocab-size 16000 # --max-epoch 1
